a3728a6e353e6804ace5903fc60949162b180c00
zhoucan1 <zhoucan1@360.cn>
2021-01-08 10:21:54 +0800

    ssr 架构基本实现

---
+++ dist/client/js/chunk-2d21a3d2.37ded7a4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"home"},[e._v("首页")])},s=[],c={name:"Home"},l=c,u=t("2877"),o=Object(u["a"])(l,a,s,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.37ded7a4.js.map
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+const Home = () => import('../views/Home.vue');
+const About = () => import('../views/About.vue');
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
-})
-
-export default router
+// 工厂函数——创建新的路由实例
+export default function createRouter () {
+  return new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/about',
+        name: 'about',
+        component: About,
+      },
+    ]
+  })
+}

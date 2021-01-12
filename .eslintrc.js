c7583cbea6c692210d880b7975783b10f047cf7a
zhoucan1 <zhoucan1@360.cn>
2021-01-12 11:24:31 +0800

    eslint airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/airbnb',
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'global-require': 0,
+    'linebreak-style': 0,
+    "no-irregular-whitespace": 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'camelcase': 0,
+    'camelcase': 0,
+    'max-len': ["error", {
+      "code": 200
+    }],
+    'no-eval': 0,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};

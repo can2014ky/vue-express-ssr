92fca8599b4e289dc01a526df66260b3ce1934bb
zhoucan1 <zhoucan1@360.cn>
2021-01-14 18:57:31 +0800

    服务端开发环境配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-const Home = () => import('../views/Home.vue');
-const About = () => import('../views/About.vue');
+const Home = () => import('../views/Home.vue')
+const About = () => import('../views/About.vue')
 
 Vue.use(Router)
 
@@ -13,13 +13,13 @@ export default function createRouter () {
       {
         path: '/',
         name: 'home',
-        component: Home,
+        component: Home
       },
       {
         path: '/about',
         name: 'about',
-        component: About,
-      },
+        component: About
+      }
     ]
   })
 }

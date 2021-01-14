7f8c7bdff526b68dbdc4d3cb356b13cc4121fca2
zhoucan1 <zhoucan1@360.cn>
2021-01-14 16:36:32 +0800

    客户端数据预取，解决首次切换about页时报错未渲染数据的问题

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,28 +1,28 @@
-import Vue from 'vue'
+// import Vue from 'vue'
 import createApp from './main'
 
-Vue.mixin({
-  beforeMount () {
-    const { asyncData } = this.$options
-    if (asyncData) {
-      this.dataPromise = asyncData({
-        store: this.$store,
-        route: this.$route
-      })
-    }
-  },
-  beforeRouteUpdate (to, from, next) {
-    const { asyncData } = this.$options
-    if (asyncData) {
-      asyncData({
-        store: this.$store,
-        route: to
-      }).then(next).catch(next)
-    } else {
-      next()
-    }
-  }
-})
+// Vue.mixin({
+//   beforeMount () {
+//     const { asyncData } = this.$options
+//     if (asyncData) {
+//       this.dataPromise = asyncData({
+//         store: this.$store,
+//         route: this.$route
+//       })
+//     }
+//   },
+//   beforeRouteUpdate (to, from, next) {
+//     const { asyncData } = this.$options
+//     if (asyncData) {
+//       asyncData({
+//         store: this.$store,
+//         route: to
+//       }).then(next).catch(next)
+//     } else {
+//       next()
+//     }
+//   }
+// })
 
 const { app, router, store } = createApp()
 
@@ -30,5 +30,28 @@ if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
 router.onReady(() => {
+  router.beforeResolve((to, from, next) => {
+    const matched = router.getMatchedComponents(to)
+    const prevMatched = router.getMatchedComponents(from)
+
+    // 找出两个匹配列表的差异组件，不做重复的数据读取工作
+    let diffed = false
+    const activated = matched.filter((c, i) => {
+      return diffed || (diffed = (prevMatched[i] !== c))
+    })
+    if (!activated.length) {
+      return next()
+    }
+    Promise.all(activated.map(c => {
+      if (c.asyncData) {
+        return c.asyncData({
+          store,
+          route: to
+        })
+      }
+    })).then(() => {
+      next()
+    }).catch(next)
+  })
   app.$mount('#app')
 })

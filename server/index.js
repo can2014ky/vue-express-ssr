cb6dc17a998f774fa97251856aaffa7fd51baf19
zhoucan1 <zhoucan1@360.cn>
2021-01-15 13:18:34 +0800

    serve-favicon 中间件处理favicon显示问题

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const fs = require('fs')
+const path = require('path')
+const favicon = require('serve-favicon')
 
 const app = express();
 
 // 中间件处理静态文件请求
+const resolve = file => path.resolve(__dirname, file);
 app.use(express.static('../dist/client', {index: false}))
+// app.use(express.static(resolve('../public')));
 
 const isDev = process.env.SSR_ENV === 'dev';
+if (isDev) {
+  app.use(favicon('public/favicon.ico'))
+} else {
+  app.use(favicon('../dist/client/favicon.ico'))
+}
 const router = isDev ? require('./ssr.dev') : require('./ssr.prod')
 
 app.use('', router);
